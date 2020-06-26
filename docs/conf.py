9bd2c30f8b352e163fbfaa3072b807f6817d57ad
Thien-Nam <thiennam.tnd@gmail.com>
2020-06-25 23:20:41 -0600

    attempt to add readthedocs submodules

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-if 'READTHEDOCS' not in os.environ:
-    import cython_generated_ext
-
 import ibots
 
 # -- General configuration ---------------------------------------------
